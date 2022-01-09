37abcbcba5fafa93847ee9a192ff3734b0b5ec57
unknown <saranshmehta96@gmail.com>
2022-01-09 14:42:24 +0530

    Final Commit

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,7 @@ if(currUrl.slice(0,5) == "https") {
     baseFrontend = "https://saranshm.github.io/letsendorse_assn/public/html"
 }
 else {
-    baseBackend = "http://localhost:3000"
+    baseBackend = "https://letsendorse-assn.herokuapp.com"
     baseFrontend = "file:///D:/letsendorse_assn/public/html"
 }
 
